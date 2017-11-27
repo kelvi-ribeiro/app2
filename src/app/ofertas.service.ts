44b873af437ebb3d2d58caf23e24e38564ddcaef
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-11-27 15:00:46 -0200

    Utilizando mais Promises para exemplificar

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,8 +1,8 @@
-import {Oferta} from './shared/oferta.model'
-export class OfertasService{
+import { Oferta } from './shared/oferta.model'
 
-    public ofertas:Array<Oferta> =
-    [
+export class OfertasService {
+
+    public ofertas: Oferta[] = [
         {
             id: 1,
             categoria: "restaurante",
@@ -52,35 +52,39 @@ export class OfertasService{
             ]
         }
     ]
-
-    public getOfertas():Array<Oferta>{
+    
+    public getOfertas(): Array<Oferta> {
         return this.ofertas
     }
-    //public getOfertas2():Promise<Array<Oferta>> Outro jeito
-    public getOfertas2():Promise<Oferta[]>{
-        return new Promise((resolve,reject)=>{
-            //Algum tipo de processamento, que ao finalizar, chama a função resolve ou a função reject
+
+    public getOfertas2(): Promise<Oferta[]> {
+        return new Promise((resolve, reject) => {
+            //algum tipo de processamento, que ao finalizar, chama a função resolve ou a função reject
+            //console.log('será que passou por aqui?')
             let deu_certo = true
-            if(deu_certo){
-                setTimeout(()=> resolve(this.ofertas),3000)
             
-            }
-            else{
-                reject({codigo_error:404,mensagem_erro:'Not found XYZ'})
+            if(deu_certo) {
+                setTimeout(() => resolve( this.ofertas ), 3000)
+                
+            } else {
+                reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado XYZ' })
             }
         })
-
-        .then((ofertas:Oferta[])=>{
+        .then(( ofertas: Oferta[]) => {
             //fazer alguma tratativa
             console.log('primeiro then')
             return ofertas
-
         })
-        .then((ofertas:Oferta[])=>{
+        .then(( ofertas: Oferta[]) => {
+            //fazer uma outra tratativa
             console.log('segundo then')
+            return new Promise((resolve2, reject2) => {
+                setTimeout(() => { resolve2( ofertas ) },3000)
+            })
+        })
+        .then(( ofertas: Oferta[] ) => {
+            console.log('terceiro then executado após 3 segundos porque estava aguardando uma promisse ser resolvida')
             return ofertas
         })
-
     }
-
 }
