44f9fced3a622968f1f2a66832570bb9496779c1
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-03 10:10:07 -0200

    Injetando o serviço HTTP do Angular como depência de outros serviços

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,12 @@
+import {Http} from '@angular/http'
+import {Injectable} from '@angular/core'
 import { Oferta } from './shared/oferta.model'
 
+@Injectable()
 export class OfertasService {
+    constructor(private http:Http){
+
+    }
 
     public ofertas: Oferta[] = [
         {
