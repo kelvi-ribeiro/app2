008192dbb4753ae24bfcd91b4bd669929a6f06a5
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-16 21:09:37 -0200

    Aula 153. Lógica da pesquisa de ofertas com Subject e switchMap para tratar das pesquisas etapa por etapa

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {OfertasService} from '../ofertas.service'
 import { Observable } from 'rxjs/Observable';
 import {Oferta} from '../shared/oferta.model'
+import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/operator/switchMap'
 
 @Component({
   selector: 'app-topo',
@@ -11,20 +14,29 @@ import {Oferta} from '../shared/oferta.model'
 })
 export class TopoComponent implements OnInit {
 
+
   public ofertas:Observable<Oferta[]>
+  private subjectPesquisa:Subject<string> = new Subject<string>()
 
   constructor(private ofertasService:OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa //retorno Oferta[]
+    .switchMap((termo:string) => {
+      console.log('requisição http para a api: ' , termo)
+      return this.ofertasService.pesquisaOfertas(termo)
+
+    })
+
+    this.ofertas.subscribe((ofertas:Oferta[])=>{
+      console.log(ofertas)
+    })
   }
 
   public pesquisa(termoDaBusca:string):void{
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
-    this.ofertas.subscribe(
-    (ofertas:Oferta[])=>console.log(ofertas),
-    (erro:any)=>console.log('erro de status:',erro.status),
-    ()=> console.log ('Fluxo de eventos completo!')
-    )
+    console.log('keyup caracter: ' , termoDaBusca)
+    this.subjectPesquisa.next(termoDaBusca)
+    
   }
 
 }
