130719f9da6d60ebd4fa372d54293b49e9af9e21
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-18 00:29:11 -0200

    Aula 165. Aplicando internacionalização e meus próprios ajutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpModule} from '@angular/http'
-import {RouterModule} from '@angular/router'
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpModule } from '@angular/http'
+import { RouterModule } from '@angular/router'
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
+import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
@@ -9,29 +14,31 @@ import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
-import {ROUTES} from './app.routes';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
-import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component'
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent,    
+    RodapeComponent,
     RestaurantesComponent,
     DiversaoComponent,
     OfertaComponent,
     ComoUsarComponent,
-    OndeFicaComponent
+    OndeFicaComponent,
+    
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'pt-BR' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
