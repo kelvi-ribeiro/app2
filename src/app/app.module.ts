98a53b69335e35798133d8697af9e4e55e753b3c
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-19 05:34:28 -0200

    Aula 187. Criando o componente OrdemCompraSucesso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 //pipe
 import{DescricaoReduzida} from './util/descricao-reduzida.pipe';
-import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component'
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
+import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component'
 
 
 
@@ -37,7 +38,8 @@ import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component'
     ComoUsarComponent,
     OndeFicaComponent,
     DescricaoReduzida,
-    OrdemCompraComponent
+    OrdemCompraComponent,
+    OrdemCompraSucessoComponent
     
   ],
   imports: [
