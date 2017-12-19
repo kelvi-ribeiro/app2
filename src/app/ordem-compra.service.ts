2cdd91db8493bec755642d9ce79f24e47940f662
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-19 04:31:49 -0200

    Aula 185. Injetando o serviço HTTP do angular em OrdemCompraService

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,5 +1,12 @@
+import {Injectable} from '@angular/core'
+import {Http} from '@angular/http'
 import {Pedido} from './shared/pedido.model'
+
+@Injectable()
 export class OrdemCompraService{
+    constructor(private http:Http){
+
+    }
     public efetivarCompra(pedido:Pedido):void{
         console.log(pedido)
     }
