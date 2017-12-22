abc197e9cabf8c83f292acba79bb12fc80a390a5
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-22 18:10:09 -0200

    Aula 215. Atribundo CarrinhoService ao componente Oferta

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,7 +1,12 @@
  import {ItemCarrinho} from './shared/item-carrinho.model'
  class CarrinhoService {
+     
      public itens:ItemCarrinho[] = []
-
+     public item:ItemCarrinho
+     public exibirItens():ItemCarrinho[]{
+        return this.itens
+    }
+   
  }
 
  export default CarrinhoService
