51bdebbc53460344208389fcfd8a96fd86db68fc
Kelvi Martins <kelvi.ribeiro@unigranrio.br>
2017-12-23 18:06:48 -0200

    Aula 227. Enviando pedido com itens de carrinho para a API REST(ASSOCIANDO itens do carrinho co mo pedido)

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -54,6 +54,7 @@ export class OrdemCompraComponent implements OnInit {
         this.formulario.value.numero,
         this.formulario.value.complemento,
         this.formulario.value.formaPagamento
+        this.carrinhoService.exibirItens()
       )
      this.ordemCompraService.efetivarCompra(pedido)
      .subscribe((idPedido:number)=>{
